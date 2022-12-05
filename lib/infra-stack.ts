b2d015b2e5093364270a71d71e18ecfb36f3642a
Brandon Ly <brandonly@lostcoding.com>
2022-12-05 17:14:45 -0600

    refactor project structure

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,11 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const indexFunction = new NodejsFunction(this, 'index-function', {
+      entry: './src/handlers/index.ts',
+    });
     // The code that defines your stack goes here
 
     // example resource
